c820af9c090bd8638bffb376c927decfa915b66b
pengelana <>
2020-03-17 15:28:38 +0800

    Capslock to ESC

--- keyboard.sh
+++ keyboard.sh
@@ -2,6 +2,8 @@
 
 HIDUTIL=/usr/bin/hidutil
 
-$HIDUTIL property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x7000000E6,"HIDKeyboardModifierMappingDst":0x7000000E4}]}' > /dev/null 2>&1
+$HIDUTIL property --set '{"UserKeyMapping":[
+    {"HIDKeyboardModifierMappingSrc":0x7000000E6,"HIDKeyboardModifierMappingDst":0x7000000E4},
+    {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}'
 
 ## hidutil property --get "UserKeyMapping"
